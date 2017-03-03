b2829d326511fd39815fd5979ccf89499de2ac61
Federico G. Schwindt <fgsch@lodoss.net>
2017-03-03 10:58:25 +0000

    Move the test outside travis

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Run every snippet through varnishd
+
+for f in $(echo v${1%%.?}/*.vcl); do
+	t=$(mktemp)
+	cat >$t <<_EOF
+vcl 4.0;
+backend default { .host = "127.0.0.1"; }
+include "$(pwd)/$f";
+_EOF
+	varnishd -C -f $t || exit 2
+done
