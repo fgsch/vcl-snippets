8eb429a42249a6c466771dd1251b8d7168140b20
Federico G. Schwindt <fgsch@lodoss.net>
2017-03-03 11:29:12 +0000

    More corrections

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ vcl 4.0;
 backend default { .host = "127.0.0.1"; }
 include "$(pwd)/$f";
 _EOF
-	echo -n "$f "
+	echo -n "Snippet $f "
 	varnishd -n $(pwd) -C -f $t 2>/dev/null
 	if [ $? -eq 0 ]; then
 		echo "passed"
 	else
 		echo "FAILED"
-		status=$?
+		status=2
 	fi
 done
 exit $status
